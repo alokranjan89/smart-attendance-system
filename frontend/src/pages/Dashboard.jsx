2ea8f80e4bd541e4d245c26128c2da7aeff0ff7b
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-12-23 15:01:19 +0530

    added mark attendance button

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,21 +2,39 @@ import React, { useEffect, useState } from 'react'
 import StatCard from '../components/ui/StatCard'
 import AttendanceTable from '../components/table/AttendanceTable'
 import { fetchAttendance } from '../services/attendanceService'
+import { captureAttendance } from "../services/mlService";
+import CaptureButton from "../components/ui/CaptureButton";
 
 const Dashboard = () => {
 
     const [attendance, setAttendance] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        loadData();
-        const interval = setInterval(loadData, 5000);
-        return () => clearInterval(interval);
-    },[]);
+ 
 
     async function loadData() {
         const data = await fetchAttendance();
         setAttendance(data);
     }
+async function handleCapture() {
+    try {
+      setLoading(true);
+      await captureAttendance();   
+      await loadData();            
+    } catch (err) {
+      alert("Failed to capture attendance");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+       useEffect(() => {
+        loadData();
+        const interval = setInterval(loadData, 5000);
+        return () => clearInterval(interval);
+    },[]);
+
+
 
     const total = attendance.length;
     const present = attendance.filter(a => a.name !== "Unknown" && a.name !== "Spoof Detected").length;
@@ -24,6 +42,9 @@ const Dashboard = () => {
     const spoof = attendance.filter(a => a.name === "Spoof Detected").length;
   return (
     <div className='p-6 max-w-7xl mx-auto'>
+        <div className="flex justify-end mb-4">
+        <CaptureButton onClick={handleCapture} loading={loading} />
+      </div>
       <div className='grid grid-cols-1 md:grid-cols-4 gap-4 mb-6'>
         <StatCard title="Total Entries" value={total} color="bg-blue-600"/>
         <StatCard title="Present" value={present} color="bg-green-600"/>
