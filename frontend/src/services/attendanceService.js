ef4c09452bceabbad06235cefa06402ee98b4419
ANKIT KUMAR <ankitmom0393@gmail.com>
2025-12-23 13:52:53 +0530

    build api logic to fetch attendance

---
+++ frontend/src/services/attendanceService.js
@@ -0,0 +1,7 @@
+const API_URL = "http://localhost:8000/attendance";
+
+export async function fetchAttendance() {
+    const res = await fetch(API_URL);
+    if(!res.ok) throw new Error("Failed to fetch attendance");
+    return res.json();
+}
